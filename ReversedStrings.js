7455d3008b3963376c6c631f9c6ac4b2f1f90a0a
amilord <60766420+amilord@users.noreply.github.com>
2021-03-02 23:56:31 -0500

    Codewar020222

---
+++ ReversedStrings.js
@@ -0,0 +1,13 @@
+// Complete the solution so that it reverses the string passed into it.
+//
+// 'world'  =>  'dlrow'
+// function solution(str){
+ // return str.split('').reverse().join('');
+//
+// }
+
+// sample test
+// Test.assertEquals(solution('world'), 'dlrow')
+// Test.assertEquals(solution('hello'), 'olleh')
+// Test.assertEquals(solution(''), '')
+// Test.assertEquals(solution('h'), 'h')
