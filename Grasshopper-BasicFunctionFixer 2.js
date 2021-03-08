a3db5c3d05227dbc0ceeb5460ac32a4ac1e8f08e
amilord <60766420+amilord@users.noreply.github.com>
2021-03-07 19:54:39 -0500

    codewar030721

---
+++ Grasshopper-BasicFunctionFixer 2.js
@@ -0,0 +1,42 @@
+Fix the function
+I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+
+Can you help me fix the function?
+
+
+
+
+
+
+
+
+function addFive(num) {
+  var total = num + 5
+  return num + 5
+}
+
+
+
+
+Sample Tests:
+describe('fix add five', () => {
+  it('fixed tests', () => {
+    Test.assertEquals(addFive(5), 10)
+    Test.assertEquals(addFive(0), 5)
+    Test.assertEquals(addFive(-5), 0)
+  })
+})
+1
+describe('fix add five', () => {
+2
+  it('fixed tests', () => {
+3
+    Test.assertEquals(addFive(5), 10)
+4
+    Test.assertEquals(addFive(0), 5)
+5
+    Test.assertEquals(addFive(-5), 0)
+6
+  })
+7
+})
