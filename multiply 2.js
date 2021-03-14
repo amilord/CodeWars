13098e51ac52ee8f2b266a9d90d1e0a515e717c6
amilord <60766420+amilord@users.noreply.github.com>
2021-03-13 20:11:32 -0500

    031321

---
+++ multiply 2.js
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/50654ddff44f800200000004/train/javascript
+// This code does not execute properly. Try to figure out why.
+//
+// function multiply(a, b){
+//  return a * b
+// }
+// sample Test:
+// const assert = require("chai").assert;
+//
+// describe("Multiply", () => {
+//   it("fixed tests", () => {
+//     assert.strictEqual(multiply(1,1), 1);
+//     assert.strictEqual(multiply(2,1), 2);
+//     assert.strictEqual(multiply(2,2), 4);
+//     assert.strictEqual(multiply(3,5), 15);
+//   });
+// });
