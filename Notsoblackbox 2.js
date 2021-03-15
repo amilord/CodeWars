ce72c0dfbf810419c32920c7f31d8c849c6e7818
amilord <60766420+amilord@users.noreply.github.com>
2021-03-14 20:19:07 -0400

    codewar031421

---
+++ Notsoblackbox 2.js
@@ -0,0 +1,27 @@
+// // Not so black box
+//
+// The kata has been preloaded with a function called blackBox. Figure out what the function does.
+//
+// You may need to think laterally!
+//
+//
+
+
+
+
+// blackBox();
+// console.log(blackBox());
+//
+// JSopenSesame()
+//
+
+
+
+
+
+
+// Test.describe("Did you open the box?", function() {
+//
+//     Test.expect(check(), "It's clamped shut!");
+//
+// });
