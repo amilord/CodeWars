4eb3b3d4a2ef83427e0160a780bc4412de91bc41
amilord <60766420+amilord@users.noreply.github.com>
2021-03-15 20:20:55 -0400

    codewar031521

---
+++ Oppositenumber 2.js
@@ -0,0 +1,14 @@
+// Very simple, given a number, find its opposite.
+//
+// Examples:
+//
+//
+// function opposite(number) {
+// return -number
+// }
+//
+//
+//
+// Sample Tests:
+// 1
+// Test.assertEquals(opposite(1), -1,)
