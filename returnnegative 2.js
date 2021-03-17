f7f47b4ba7b0671481af16f7bd7f7f9cea5bea10
amilord <60766420+amilord@users.noreply.github.com>
2021-03-16 20:24:51 -0400

    codewar031621

---
+++ returnnegative 2.js
@@ -0,0 +1,13 @@
+// Return Negative
+// function makeNegative(num) {
+//   // Code?
+// if (num>0){
+//     return -num
+//   }
+//   else if (num<0){
+//     return num
+//   }
+//   else {
+//     return 0
+//   }
+// }
