712182fb2846b3d85e5d6a994c9d775203a4257f
amilord <60766420+amilord@users.noreply.github.com>
2021-04-14 01:23:17 -0400

    codewar

---
+++ Mostdigits.js
@@ -0,0 +1,10 @@
+// function findLongest(array){
+//   // code here
+//   let max = 0;
+//   for(let i = 0; i < array.length; i++) {
+//     if(String(array[i]).length > String(max).length){
+//       max = array[i]
+//     }
+//   }
+//   return max
+// }
