e6636b75f935793b8c2b447c2f2b7390d3dee610
amilord <60766420+amilord@users.noreply.github.com>
2021-04-24 01:10:24 -0400

    codewars

---
+++ Noodditieshere.js
@@ -0,0 +1,25 @@
+// function noOdds( values ){
+//   // Return all non-odd values
+// //   we should return even numbers
+// //   it can be done with a filter or for loop ... i chose filter becasue for loop is a little tricky
+//   return values.filter(val => val % 2 === 0);
+//
+//
+// //   the filter method will go over all of the values for the value array and return a function
+// }
+//
+// // function noOdds( values ){
+// //   // Return all non-odd values
+// // //   we should return even numbers
+// // // example of a for loop
+//
+// //   const arr =[ ]
+// //   for (let i=0; i,values.length; i++){
+// //     if (values [i] % 2 === 0 ){
+// //       arr.push(values [i]);
+// //     }
+// //   }
+// //   return arr;
+//
+// // //   the filter method will go over all of the values for the value array and return a function
+// // }
