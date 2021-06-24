22e6ee1098891c9a8053035076e0229b257a2eaf
amilord <60766420+amilord@users.noreply.github.com>
2021-06-24 15:55:29 -0400

    CodeWar 5/17/21

---
+++ fixStringCase.js
@@ -0,0 +1,12 @@
+// function solve(s){
+//     //..
+//   let upperCase = s.split('').filter(letter => letter === letter.toUpperCase())
+//   let lowerCase = s.split('').filter(letter => letter === letter.toLowerCase())
+//
+//   if(upperCase.length > lowerCase.length){
+//       return s.toUpperCase()
+//     }
+//   else {
+//     return s.toLowerCase()
+//   }
+// }
